c7131dc6e6645d9807b6b29e05a059d02cbb5b58
Liudmylla <liudmyla12@yahoo.fr>
2021-10-18 21:29:06 +0200

    init commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,79 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+const commentData = {
+  title: "Fake article title.",
+  author: "grzm",
+  comments: [
+    {
+      id: 1,
+      text: "Example comment here.",
+      author: "user2",
+      children: [
+        {
+          id: 2,
+          text: "Another example comment text.",
+          author: "user3",
+          children: [
+            {
+              id: 3,
+              text: "Another example comment text.",
+              author: "user4",
+              children: [
+                {
+                  id: 9,
+                  text: "Another example comment text.",
+                  author: "user4",
+                  children: [],
+                },
+              ],
+            },
+          ],
+        },
+      ],
+    },
+    {
+      id: 4,
+      text: "Example comment here 2.",
+      author: "user5",
+      children: [
+        {
+          id: 99,
+          text: "Another example comment text.",
+          author: "user4",
+          children: [],
+        },
+      ],
+    },
+  ],
+};
+
+function Comment({ comment }) {
+  const onDelete = (id) => {
+    console.log(id);
+  };
+  const nestedComments = (comment.children || []).map((comment) => {
+    return (
+      <>
+        <button onClick={() => onDelete(comment.id)}>X</button>
+        <Comment key={comment.id} comment={comment} type="child" />
+      </>
+    );
+  });
+
+  return (
+    <div style={{ marginLeft: "25px", marginTop: "10px" }}>
+      <span>{comment.id}</span>
+      {nestedComments}
+    </div>
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {commentData.comments.map((comment) => {
+        return <Comment key={comment.id} comment={comment} />;
+      })}
     </div>
   );
 }
